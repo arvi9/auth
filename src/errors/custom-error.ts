a0d3a9975834d703f2ab89ae6924d3e55f563ccb
adc work <adc9work@outlook.com>
2020-08-24 23:16:08 +0530

    Authentication

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -2,12 +2,10 @@ export abstract class CustomError extends Error {
   abstract statusCode: number;
 
   constructor(message: string) {
-    //super(message) is only for logging purpose
     super(message);
 
     Object.setPrototypeOf(this, CustomError.prototype);
   }
 
-  // returns Array of Objects
   abstract serializeErrors(): { message: string; field?: string }[];
 }
